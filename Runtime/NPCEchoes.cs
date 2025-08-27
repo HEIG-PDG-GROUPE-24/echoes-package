150c08b55ee1d5317cc6da126a8f7154e918a302
Raphael Perret <rp2709@hotmail.com>
2025-08-27 15:22:04 +0200

    started implementing NPC Echoes interface

--- Runtime/NPCEchoes.cs
+++ Runtime/NPCEchoes.cs
@@ -5,9 +5,51 @@ namespace Echoes
 {
     public class NPCEchoes : MonoBehaviour
     {
+
+        [InlineEditor] public NPCSo npcData;
+
+        public Dictionary<string,double> OpinionOfplayer { private set; get; }
+        private Dictionary<string, double> personality;
+        private Dictionary<string, double> informtantsTrust;
+
+        public bool InPlayerInteraction { private set; get; }
+        public bool AcceptsInterferenceDuringInteraction {private set; get;}
         
-        [InlineEditor]
-        public NPCSo npcData;
+        public double OpinionOfPlayerRegarding(string traitName)
+        {
+            return OpinionOfPlayer[traitName];
+        }
+
+        public double TrustTowards(NPCEchoes other)
+        {
+            return informantsTrust[other.npcData.Name];
+        }
+
+        public double AppreciationOfPlayer()
+        {
+            // calculate a score based on current opinion and personality
+        }
+
+        public bool StartPlayerInteraction(bool allowInterference = false)
+        {
+            if InPlayerInteraction return false;
+            AcceptsInterferenceDuringInteraction = allowInterference;
+            InPlayerInteraction = true;
+            return true;
+        }
+
+        public void EndPlayerInteraction()
+        {
+            // For each contact, give them our opinion of the player
+        }
+
+        protected bool ReceiveOpinion(NPCEchoes from)
+        {
+            if (InPlayerInteraction && !AcceptsInterferenceDuringInteraction) return false;
+            //adjust current opinion
+        }
+        
+        
         
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
