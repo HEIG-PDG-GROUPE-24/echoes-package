e9380790040502b235125beb628b1811535b3dc9
Raphael Perret <rp2709@hotmail.com>
2025-08-29 11:23:21 +0200

    Filling contacts Set when loading SO and adding informants with default trust

--- Runtime/NPCComponent.cs
+++ Runtime/NPCComponent.cs
@@ -9,45 +9,43 @@ namespace Echoes.Runtime
 {
     public class NPCEchoes : MonoBehaviour
     {
-        
-        [InlineEditor]
-        public NPC npcData;
+        [InlineEditor] public NPC npcData;
 
-        public Dictionary<string,double> OpinionOfPlayer { private set; get; } =  new Dictionary<string, double>();
-        public Dictionary<string, double> Personality {private set; get;} = new Dictionary<string, double>();
-        public Dictionary<string, double> InformantsTrust {private set; get; } = new Dictionary<string, double>();
+        public Dictionary<string, double> OpinionOfPlayer { private set; get; } = new Dictionary<string, double>();
+        public Dictionary<string, double> Personality { private set; get; } = new Dictionary<string, double>();
+        public Dictionary<string, double> InformantsTrust { private set; get; } = new Dictionary<string, double>();
         public HashSet<NPCEchoes> Contacts { private set; get; } = new HashSet<NPCEchoes>();
 
         public bool InPlayerInteraction { private set; get; }
-        public bool AcceptsInterferenceDuringInteraction {private set; get;}
-        
+        public bool AcceptsInterferenceDuringInteraction { private set; get; }
+
         /**
          * Loads npc state from serializable data
          * @param data serializable data to load
          */
         public void LoadFromData(EchoesNpcData data)
         {
-            if(npcData != null && npcData.name != data.name)
+            if (npcData != null && npcData.name != data.name)
                 throw new ArgumentException("Name of NPC doesn't match");
 
-            bool dataIsComplete = true; 
-            
-            OpinionOfPlayer = new Dictionary<string, double>();
+            bool dataIsComplete = true;
+
+            OpinionOfPlayer.Clear();
             if (data.opinionOfPlayer != null)
                 foreach (var trait in data.opinionOfPlayer)
                     OpinionOfPlayer.Add(trait.traitName, trait.value);
             else dataIsComplete = false;
 
-            Personality = new Dictionary<string, double>();
-            if(data.npcPersonality != null)
+            Personality.Clear();
+            if (data.npcPersonality != null)
                 foreach (var trait in data.npcPersonality)
                     Personality.Add(trait.traitName, trait.value);
             else dataIsComplete = false;
-            
-            InformantsTrust =  new Dictionary<string, double>();
-            if(data.trustLevels != null)
+
+            InformantsTrust.Clear();
+            if (data.trustLevels != null)
                 foreach (var trust in data.trustLevels)
-                    InformantsTrust.Add(trust.informantName,trust.level);
+                    InformantsTrust.Add(trust.informantName, trust.level);
             else dataIsComplete = false;
 
             if (!dataIsComplete)
@@ -59,17 +57,29 @@ namespace Echoes.Runtime
          */
         public void LoadFromSo()
         {
-            OpinionOfPlayer = new Dictionary<string, double>();
+            OpinionOfPlayer.Clear();
             foreach (var trait in npcData.Opinions)
                 OpinionOfPlayer.Add(trait.Name, trait.Intensity);
 
-            Personality = new Dictionary<string, double>();
+            Personality.Clear();
             foreach (var trait in npcData.Traits)
                 Personality.Add(trait.Name, trait.Intensity);
-            
-            InformantsTrust =  new Dictionary<string, double>();
+
+            InformantsTrust.Clear();
             foreach (var trust in npcData.Trusts)
-                InformantsTrust.Add(trust.Contact.name,trust.TrustLevel);
+                InformantsTrust.Add(trust.Contact.name, trust.TrustLevel);
+
+            Contacts.Clear();
+            foreach (var contact in npcData.Contacts)
+            {
+                Contacts.Add(contact);
+            }
+
+            foreach (var group in NPCGlobalStatsGeneratorSo.Instance.globalGroupes.Groupes.Where(group =>
+                         group.Members.Contains(this)))
+            {
+                foreach (var member in group.Members.Where(member => member != this)) Contacts.Add(member);
+            }
         }
 
         /**
@@ -81,10 +91,11 @@ namespace Echoes.Runtime
          */
         private double Normalize(double x, double min, double max)
         {
-            if (x < min || x > max) throw new ArgumentOutOfRangeException(nameof(x),x,"value must be between min and max inclusive");
+            if (x < min || x > max)
+                throw new ArgumentOutOfRangeException(nameof(x), x, "value must be between min and max inclusive");
             return (x - min) / (max - min) * 2 - 1;
         }
-        
+
         public double OpinionOfPlayerRegarding(string traitName)
         {
             return OpinionOfPlayer[traitName];
@@ -103,10 +114,12 @@ namespace Echoes.Runtime
             double score = 0;
             foreach (var trait in Personality.Keys)
             {
-                double maxDiff = 1; // max - min
+                double maxDiff = NPCGlobalStatsGeneratorSo.Instance.globalTraits.maxValue -
+                                 NPCGlobalStatsGeneratorSo.Instance.globalTraits.minValue;
                 double diff = Math.Abs(OpinionOfPlayer[trait] - Personality[trait]);
-                score -= Normalize(diff,0,maxDiff) / Personality.Count;
+                score -= Normalize(diff, 0, maxDiff) / Personality.Count;
             }
+
             return score;
         }
 
@@ -129,32 +142,36 @@ namespace Echoes.Runtime
         protected bool ReceiveOpinion(NPCEchoes from)
         {
             if (InPlayerInteraction && !AcceptsInterferenceDuringInteraction) return false;
+            if (!InformantsTrust.ContainsKey(from.npcData.name))
+                InformantsTrust.Add(from.npcData.name,
+                    (NPCGlobalStatsGeneratorSo.Instance.globalTraits.minValue +
+                     NPCGlobalStatsGeneratorSo.Instance.globalTraits.maxValue) / 2);
+
             //adjust current opinion
             double trustLevel = TrustTowards(from);
             foreach (var trait in OpinionOfPlayer.Keys.ToList())
             {
                 double average = (OpinionOfPlayer[trait] + from.OpinionOfPlayer[trait]) / 2;
                 double diff = average - OpinionOfPlayer[trait];
-                OpinionOfPlayer[trait] += diff * ((Normalize(trustLevel, 0, 10)+1)/2);
+                OpinionOfPlayer[trait] += diff * ((Normalize(trustLevel,
+                    NPCGlobalStatsGeneratorSo.Instance.globalTraits.minValue,
+                    NPCGlobalStatsGeneratorSo.Instance.globalTraits.maxValue) + 1) / 2);
             }
-            
+
             return true;
         }
-        
-        
-        
+
+
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
-        
         }
 
         // Update is called once per frame
         void Update()
         {
-        
         }
-        
+
         public override string ToString() => npcData != null ? npcData.Name : "No NPC Data";
     }
 }
--- Tests/Play/NpcBehaviourTest.cs
+++ Tests/Play/NpcBehaviourTest.cs
@@ -9,84 +9,101 @@ namespace Tests.Play
 {
     public class NpcBehaviourTest
     {
-
         private GameObject goNpc1;
         private GameObject goNpc2;
         private NPCEchoes npc1;
         private NPCEchoes npc2;
-        
+
         [SetUp]
         public void SetUp()
         {
-            
             goNpc1 = new GameObject();
-            goNpc2 =  new GameObject();
-            
+            goNpc2 = new GameObject();
+
             npc1 = goNpc1.AddComponent<NPCEchoes>();
             npc2 = goNpc2.AddComponent<NPCEchoes>();
 
             npc1.npcData = ScriptableObject.CreateInstance<NPC>();
             npc2.npcData = ScriptableObject.CreateInstance<NPC>();
-            
+
             npc1.npcData.name = "npc1";
             npc2.npcData.name = "npc2";
 
-            npc1.Personality["test"] = 0.5;
-            npc2.Personality["test"] = 0.7;
-            
-            npc1.OpinionOfPlayer["test"] = 0.3;
-            npc2.OpinionOfPlayer["test"] = 0.7;
-            
+            npc1.Personality["test"] =
+                (NPCGlobalStatsGeneratorSo.Instance.globalTraits.maxValue -
+                 NPCGlobalStatsGeneratorSo.Instance.globalTraits.minValue) * 0.5 +
+                NPCGlobalStatsGeneratorSo.Instance.globalTraits.minValue;
+            npc2.Personality["test"] =
+                (NPCGlobalStatsGeneratorSo.Instance.globalTraits.maxValue -
+                 NPCGlobalStatsGeneratorSo.Instance.globalTraits.minValue) * 0.7 +
+                NPCGlobalStatsGeneratorSo.Instance.globalTraits.minValue;
+
+            npc1.OpinionOfPlayer["test"] =
+                (NPCGlobalStatsGeneratorSo.Instance.globalTraits.maxValue -
+                 NPCGlobalStatsGeneratorSo.Instance.globalTraits.minValue) * 0.3 +
+                NPCGlobalStatsGeneratorSo.Instance.globalTraits.minValue;
+            npc2.OpinionOfPlayer["test"] =
+                (NPCGlobalStatsGeneratorSo.Instance.globalTraits.maxValue -
+                 NPCGlobalStatsGeneratorSo.Instance.globalTraits.minValue) * 0.7 +
+                NPCGlobalStatsGeneratorSo.Instance.globalTraits.minValue;
+
             npc1.Contacts.Add(npc2);
             npc2.Contacts.Add(npc1);
         }
-        
+
         [TearDown]
         public void TearDown()
         {
-            
         }
 
         [Test]
         public void OpinionReception()
         {
-            npc2.InformantsTrust["npc1"] = 10;
+            npc2.InformantsTrust["npc1"] = NPCGlobalStatsGeneratorSo.Instance.globalTrust.maxValue;
             npc1.EndPlayerInteraction();
-            Assert.AreEqual(0.5,npc2.OpinionOfPlayer["test"], double.Epsilon);
+            Assert.AreEqual(
+                (NPCGlobalStatsGeneratorSo.Instance.globalTraits.maxValue -
+                 NPCGlobalStatsGeneratorSo.Instance.globalTraits.minValue) * 0.5 +
+                NPCGlobalStatsGeneratorSo.Instance.globalTraits.minValue,
+                npc2.OpinionOfPlayer["test"], 0.0001);
         }
 
         [Test]
         public void OpinionReceptionNoTrust()
         {
-            npc2.InformantsTrust["npc1"] = 0;
+            npc2.InformantsTrust["npc1"] = NPCGlobalStatsGeneratorSo.Instance.globalTrust.minValue;
             npc1.EndPlayerInteraction();
-            
-            Assert.AreEqual(0.7,npc2.OpinionOfPlayer["test"], double.Epsilon);
+
+            Assert.AreEqual(
+                (NPCGlobalStatsGeneratorSo.Instance.globalTraits.maxValue -
+                 NPCGlobalStatsGeneratorSo.Instance.globalTraits.minValue) * 0.7 +
+                NPCGlobalStatsGeneratorSo.Instance.globalTraits.minValue,
+                npc2.OpinionOfPlayer["test"], 0.0001);
         }
 
         [Test]
         public void PlayerScore1()
         {
-            Assert.AreEqual(npc1.AppreciationOfPlayer(),0.6,double.Epsilon);
+            Assert.AreEqual(npc1.AppreciationOfPlayer(), 0.6, 0.0001);
         }
-        
+
         [Test]
         public void PlayerScore2()
         {
-            Assert.AreEqual(npc2.AppreciationOfPlayer(),1,double.Epsilon);
+            Assert.AreEqual(npc2.AppreciationOfPlayer(), 1, 0.0001);
         }
-        
+
         [Test]
         public void ObjectToData()
         {
             EchoesNpcData npcData = new EchoesNpcData(npc1);
-            AssertEqual(npcData,npc1);
+            AssertEqual(npcData, npc1);
         }
 
         [Test]
         public void DataToObject()
         {
-            EchoesNpcData npcData = new EchoesNpcData(new NPCEchoes(),false);
+            EchoesNpcData npcData = new EchoesNpcData(new NPCEchoes(), false);
             NPCEchoes npcObject = new NPCEchoes();
 
             npcData.npcPersonality = new TraitValue[1];
@@ -96,16 +113,16 @@ namespace Tests.Play
 
             npcObject.LoadFromData(npcData);
 
-            AssertEqual(npcData,npcObject);
+            AssertEqual(npcData, npcObject);
         }
-        
+
         private void AssertEqual(EchoesNpcData data, NPCEchoes npc)
         {
             foreach (var trait in data.opinionOfPlayer)
                 Assert.AreEqual(trait.value, npc.OpinionOfPlayerRegarding(trait.traitName));
 
             foreach (var trait in data.npcPersonality)
-                Assert.AreEqual(trait.value,npc.Personality[trait.traitName]);
+                Assert.AreEqual(trait.value, npc.Personality[trait.traitName]);
 
             foreach (var trust in data.trustLevels)
                 Assert.AreEqual(trust.level, npc.InformantsTrust[trust.informantName]);
