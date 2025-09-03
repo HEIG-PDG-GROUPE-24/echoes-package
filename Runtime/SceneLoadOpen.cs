dd72eabfaabd38757282ec5f84da809cacda11a8
Raphael Perret <rp2709@hotmail.com>
2025-09-03 11:05:46 +0200

    Fixed reference resolving on scene load

---
+++ Runtime/SceneLoadOpen.cs
@@ -0,0 +1,56 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+#endif
+using System.Linq;
+using Echoes.Runtime;
+using Sirenix.Utilities;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Echoes.Runtime
+{
+    public static class SceneLoadOpen
+    {
+        [RuntimeInitializeOnLoadMethod]
+        private static void Init()
+        {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        private static void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            Debug.Log("Scene Loaded");
+            Object.FindObjectsByType<EchoesNpcComponent>(FindObjectsSortMode.None)
+                .Where(npc => npc.npcData != null)
+                .ForEach(npc =>
+                {
+                    npc.npcData.ResolveContactsReferences();
+                    npc.LoadFromSo();
+                });
+        }
+    }
+}
+
+#if UNITY_EDITOR
+[InitializeOnLoad]
+public static class EditorSceneWatcher
+{
+    static EditorSceneWatcher()
+    {
+        EditorSceneManager.sceneOpened += OnSceneOpened;
+    }
+
+    private static void OnSceneOpened(Scene scene, OpenSceneMode mode)
+    {
+        Debug.Log($"[Editor] Scene opened: {scene.name}");
+        Object.FindObjectsByType<EchoesNpcComponent>(FindObjectsSortMode.None)
+                .Where(npc => npc.npcData != null)
+                .ForEach(npc =>
+                {
+                    npc.npcData.ResolveContactsReferences();
+                    npc.LoadFromSo();
+                });
+    }
+}
+#endif
